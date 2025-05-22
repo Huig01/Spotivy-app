a8c9ba13eed57b62d37404782711bc340b2ecaa5
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2025-05-22 16:41:07 +0200

    Gave classes Artist and Song their properties.

--- classes/Artist.cs
+++ classes/Artist.cs
@@ -8,5 +8,9 @@ namespace Spotivy_app.classes
 {
     class Artist
     {
+        public string name;
+        private List<Album> Albums;
+        private List<Song> Songs;
+
     }
 }
--- classes/Song.cs
+++ classes/Song.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Spotivy_app.classes
 {
+    public enum Genre
+    {
+        HIPHOP,
+        ROCK,
+        JAZZ,
+        POP
+
+    }
     class Song
     {
+        public string title;
+        public List<Artist> Artists;
+        public Genre SongGenre;
+        private int Duration;
     }
 }
